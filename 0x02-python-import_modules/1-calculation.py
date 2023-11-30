e0a8e8c9a95f0665b3e48cffae24745fa750d782
NkamoN <r.nkamo@gmail.com>
2023-11-30 13:33:45 +0200

    1. My first toolbox

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
-
-#Call functions and print results
-
-sum_result = add(a, b)
-print(f"Addition: {a} + {b} = {sum_result}")
-
-difference_result = sub(a, b)
-print(f"Subtraction: {a} - {b} = {difference_result}")
-
-product_result = mul(a, b)
-print(f"Multiplication: {a} * {b} = {product_result}")
-
-division_result = div(a, b)
-print(f"Division: {a} / {b} = {division_result}")
+    a = 10
+    b = 5
+    print('{:d} + {:d} = {:d}'.format(a, b, (add(a, b))))
+    print('{:d} - {:d} = {:d}'.format(a, b, (sub(a, b))))
+    print('{:d} * {:d} = {:d}'.format(a, b, (mul(a, b))))
+    print('{:d} / {:d} = {:d}'.format(a, b, (div(a, b))))
