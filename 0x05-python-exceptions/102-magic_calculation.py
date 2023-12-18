5254663182fed570e9ab757ec589409e7994ac51
NkamoN <r.nkamo@gmail.com>
2023-12-18 21:57:29 +0200

    9. ByteCode -> Python #4

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b):
     result = 0
 
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
             if i > a:
                 raise Exception('Too far')
@@ -10,5 +10,5 @@ def magic_calculation(a, b):
         except Exception:
             result += a + b
 
-    return result
+    return (result)
 
